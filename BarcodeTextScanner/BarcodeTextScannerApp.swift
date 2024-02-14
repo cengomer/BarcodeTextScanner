2a5dae51dad9eea296a4fbeca3916c9dfa281ff2
Cengomer <ca-app-pub-3725921328240837/9585494445>
2024-02-14 13:12:33 +0300

    Initial Commit

---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by omer elmas on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
